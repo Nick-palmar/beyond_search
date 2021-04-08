a670976617f3485005f8f6ac6026244f88285987
Nicolas Palmar <nicolas.palmar8@gmail.com>
2021-04-08 01:12:31 -0400

    static serving changes

--- app.py
+++ app.py
@@ -50,13 +50,13 @@ def should_create_trie() -> bool:
     trie = get_trie()
     return len(all_repos) > 0 and trie == []
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def serve():
     return app.send_static_file('index.html')
 
-@app.errorhandler(404)
-def not_found(e):
-    return app.send_static_file('index.html')
+# @app.errorhandler(404)
+# def not_found(e):
+#     return app.send_static_file('index.html')
 
 @app.route('/api/create-trie', methods=['GET'])
 # @cross_origin()
